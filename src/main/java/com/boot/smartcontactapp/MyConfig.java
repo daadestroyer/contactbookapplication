dc7cb3bbc42643bcb03277f1e785239276841224
kool nigam <nigamshubham2000@gmail.com>
2020-10-28 23:50:42 +0530

    Error Coming..

--- src/main/java/com/boot/smartcontactapp/MyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.boot.smartcontactapp;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MyConfig {
-
-
-}
--- src/main/java/com/boot/smartcontactapp/Repo/UserRepo.java
+++ src/main/java/com/boot/smartcontactapp/Repo/UserRepo.java
@@ -2,6 +2,15 @@ package com.boot.smartcontactapp.Repo;
 
 import com.boot.smartcontactapp.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 
 public interface UserRepo extends JpaRepository<User,Integer> {
+
+    @Query("select u from User u where u.email = :email")
+    public User getUserByUserName(@Param("email") String email);
+
 }
