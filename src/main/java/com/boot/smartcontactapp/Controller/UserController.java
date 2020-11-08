16eccbfb5c52aa5d5668209b66af5c2211a78a8d
kool nigam <nigamshubham2000@gmail.com>
2020-11-08 17:17:11 +0530

    User Dashboard designing start...

--- src/main/java/com/boot/smartcontactapp/Controller/UserController.java
+++ src/main/java/com/boot/smartcontactapp/Controller/UserController.java
@@ -5,7 +5,6 @@ import com.boot.smartcontactapp.Repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -29,7 +28,7 @@ public class UserController {
         //System.out.println(user);
         model.addAttribute("user",user);
         model.addAttribute("title", "User Dashboard");
-        return "user_dashboard";
+        return "normal/user_dashboard";
     }
 
 
