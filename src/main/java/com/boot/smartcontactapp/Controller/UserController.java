a562e468f2a67e6a77b432b7cb79b5c61ef9f46e
kool nigam <nigamshubham2000@gmail.com>
2020-11-09 10:08:33 +0530

    Add Contact Form Designing Completed...

--- src/main/java/com/boot/smartcontactapp/Controller/UserController.java
+++ src/main/java/com/boot/smartcontactapp/Controller/UserController.java
@@ -1,10 +1,13 @@
 package com.boot.smartcontactapp.Controller;
 
+import com.boot.smartcontactapp.Entities.Contact;
 import com.boot.smartcontactapp.Entities.User;
 import com.boot.smartcontactapp.Repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,9 +19,8 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    // dashboard home
-    @RequestMapping("/dashboard")
-    public String dashboard(Model model , Principal principal) {
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
         String user_email = principal.getName();
         //System.out.println("Username : "+user_email);
 
@@ -26,11 +28,24 @@ public class UserController {
         User user = this.userRepository.getUserByUserName(user_email);
 
         //System.out.println(user);
-        model.addAttribute("user",user);
+        model.addAttribute("user", user);
+
+    }
+
+    // dashboard home
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
         model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
     }
 
+    // add contact handler
+    @GetMapping("/add-contact")
+    public String addContact(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact",new Contact());
+        return "normal/add-contact";
+    }
 
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,13 +1,14 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-}
+const toggleSidebar = () => {
+    if ($(".sidebar").is(":visible")) {
+        //true
+        //band karna hai
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+    } else {
+        //false
+        //show karna hai
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+    }
+};
